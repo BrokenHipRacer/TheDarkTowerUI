8426822c1c2d46cb8975cb2ca935310d7fabeeb1
aalsberge <aalsberge@homeaway.com>
2022-08-10 15:00:06 -0500

    Starter: Started the coding-blog course
    - Updated the homepage with some sample text and entries
    - I'm reconsidering my plan to change the DB

--- frontend/website/pages/_app.js
+++ frontend/website/pages/_app.js
@@ -5,6 +5,9 @@ import "../styles/layout.css"
 import "../styles/components/header.css"
 import "../styles/components/footer.css"
 
+// pages
+import  "../styles/pages/homepage.css"
+
 export default function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />
 }
--- frontend/website/pages/index.js
+++ frontend/website/pages/index.js
@@ -9,7 +9,109 @@ export default class extends Component {
             <div className="layout-wrapper">
                 <Header />
                 <div className="homepage-container">
-                    Homepage content goes here.
+                    <div className="homepage-introduction">
+                        <h1>Hello, I'm Andrew Alsberge.  Here you will find my personal works</h1>
+                        <p>I'm a full stack software developer and quality engineer.  I write about my side works and roleplaying fun.</p>
+                    </div>
+                    <div className="homepage-latest-blog-posts">
+                        <h2>
+                            Latest Blog Posts
+                            <a className="homepage-latest-blog-posts-view-all" href="/blog">View all</a>
+                        </h2>
+                        <div className="homepage-latest-blog-posts-list">
+                            <a href="/blog/post-title">
+                                <div className="homepage-latest-blog-post">
+                                    <div className="homepage-latest-thumbnail">
+                                        <img src="https://assets.coderrocketfuel.com/coding-blog-nodejs-thumbnail.png" />
+                                    </div>
+                                    <div className="homepage-latest-blog-post-title">
+                                        <h3>Your Blog Post Title</h3>
+                                    </div>
+                                </div>
+                            </a>
+                            <a href="/blog/post-title">
+                                <div className="homepage-latest-blog-post">
+                                    <div className="homepage-latest-thumbnail">
+                                        <img src="https://assets.coderrocketfuel.com/coding-blog-nodejs-thumbnail.png" />
+                                    </div>
+                                    <div className="homepage-latest-blog-post-title">
+                                        <h3>Your Blog Post Title</h3>
+                                    </div>
+                                </div>
+                            </a>
+                            <a href="/blog/post-title">
+                                <div className="homepage-latest-blog-post">
+                                    <div className="homepage-latest-thumbnail">
+                                        <img src="https://assets.coderrocketfuel.com/coding-blog-nodejs-thumbnail.png" />
+                                    </div>
+                                    <div className="homepage-latest-blog-post-title">
+                                        <h3>Your Blog Post Title</h3>
+                                    </div>
+                                </div>
+                            </a>
+                            <a href="/blog/post-title">
+                                <div className="homepage-latest-blog-post">
+                                    <div className="homepage-latest-thumbnail">
+                                        <img src="https://assets.coderrocketfuel.com/coding-blog-nodejs-thumbnail.png" />
+                                    </div>
+                                    <div className="homepage-latest-blog-post-title">
+                                        <h3>Your Blog Post Title</h3>
+                                    </div>
+                                </div>
+                            </a>
+                            <a href="/blog/post-title">
+                                <div className="homepage-latest-blog-post">
+                                    <div className="homepage-latest-thumbnail">
+                                        <img src="https://assets.coderrocketfuel.com/coding-blog-nodejs-thumbnail.png" />
+                                    </div>
+                                    <div className="homepage-latest-blog-post-title">
+                                        <h3>Your Blog Post Title</h3>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="homepage-projects">
+                        <h2>My Projects</h2>
+                        <div className="homepage-projects-list">
+                            <div className="homepage-project">
+                                <h3>
+                                    <a href="https://github.com/discourse/discourse">
+                                        <div className="homepage-project-icon">📞</div>
+                                        <div className="homepage-project-title">Discourse</div>
+                                    </a>
+                                </h3>
+                                <p>A platform for community discussion. Free, open, simple</p>
+                                <div className="homepage-project-btns">
+                                    <a className="homepage-project-view-btn" href="https://github.com/discourse/discourse">View</a>
+                                </div>
+                            </div>
+                            <div className="homepage-project">
+                                <h3>
+                                    <a href="https://github.com/nmajor25/seconds-converter">
+                                        <div className="homepage-project-icon">⌛</div>
+                                        <div className="homepage-project-title">Seconds Converter</div>
+                                    </a>
+                                </h3>
+                                <p>Convert milliseconds or seconds to days, hours, minutes, and seconds in node.js.</p>
+                                <div className="homepage-project-btns">
+                                    <a className="homepage-project-view-btn" href="https://github.com/nmajor25/seconds-converter">View</a>
+                                </div>
+                            </div>
+                            <div className="homepage-project">
+                                <h3>
+                                    <a href="https://github.com/showdownjs/showdown">
+                                        <div className="homepage-project-icon">⌛</div>
+                                        <div className="homepage-project-title">Showdown</div>
+                                    </a>
+                                </h3>
+                                <p>A bidirectional Markdown to HTML to Markdown converter written in Javascript.</p>
+                                <div className="homepage-project-btns">
+                                    <a className="homepage-project-view-btn" href="https://github.com/showdownjs/showdown">View</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <Footer />
             </div>
