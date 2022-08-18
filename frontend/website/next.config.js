f6e5fbd480236b14f711bcba0a8de91259e7d560
aalsberge <aalsberge@homeaway.com>
2022-08-18 00:32:14 -0500

    Starter: Started the coding-blog course
    - temp production url till I fix godaddy

--- frontend/website/next.config.js
+++ frontend/website/next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     env: {
         "DEV_API_URL": "http://localhost:50000",
-        "PRODUCTION_API_URL": "https://www.api.example.com"
+        "PRODUCTION_API_URL": "http://52.71.168.35/"
     }
 }
