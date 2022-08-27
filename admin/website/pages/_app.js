82d0079cb8a6018b7a8f445f9d602f342daa0875
aalsberge <aalsberge@homeaway.com>
2022-08-26 22:20:57 -0500

    Traveling: midtravel
    - login page added

--- admin/website/pages/_app.js
+++ admin/website/pages/_app.js
@@ -3,6 +3,9 @@ import "../styles/layout.css"
 import "../styles/components/header.css"
 import "../styles/components/sidebar.css"
 
+// pages
+import "../styles/login.css"
+
 export default function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />
 }
