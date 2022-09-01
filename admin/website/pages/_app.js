9676c62d8178e249882d25208c31b4100d5fae91
aalsberge <aalsberge@homeaway.com>
2022-09-01 13:16:37 -0400

    Traveling: midtravel
    - PROBLEM! can't use codemirror with node 18 so I had for find a sub.  I found one I think BUT I didn't get to download the module before getting offline so can't test this solution yet.

--- admin/website/pages/_app.js
+++ admin/website/pages/_app.js
@@ -6,6 +6,7 @@ import "../styles/components/sidebar.css"
 // pages
 import "../styles/login.css"
 import "../styles/blog/index.css"
+import "../styles/blog/create-new-post.css"
 
 export default function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />
