2f5032bde74317467fa01e528e1a02542888ac68
aalsberge <aalsberge@homeaway.com>
2022-09-02 22:36:14 -0400

    Traveling: Cruise
    - admin api start and oops forgot author

---
+++ admin/rest-api/index.js
@@ -0,0 +1,47 @@
+const express = require("express")
+const helmet = require("helmet")
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+const cors = require("cors")
+const bodyParser = require("body-parser")
+const cookieParser = require("cookie-parser")
+
+const config = require("./config.js")
+
+dotenv.config()
+
+const PORT = process.env.PORT || 5001
+
+const app = express()
+
+const mongoString = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@alsbergeblogatlastemp.ickwxsm.mongodb.net/blog?retryWrites=true&w=majority`
+
+mongoose.connect(mongoString, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+
+mongoose.connection.on("error", function(err) {
+    if (process.env.NODE_ENV === "development") {
+        console.log(err)
+    }
+})
+
+mongoose.connection.on("open", function() {
+    if (process.env.NODE_ENV === "development") {
+        console.log("Connected to database.")
+    }
+})
+
+app.use(helmet())
+
+app.use(cors({
+    origin: process.env.NODE_ENV === "development" ? config.devAdminURL : /admin.alsberge.dev$/,
+    credentials: true
+}))
+
+app.use(bodyParser.json({limit: "50mb"}))
+app.use(bodyParser.urlencoded({ extended: false, limit: "50mb" }))
+
+app.use(cookieParser())
+
+app.listen(PORT, function () {
+    console.log(`Express app listening on port ${PORT}`)
+})
