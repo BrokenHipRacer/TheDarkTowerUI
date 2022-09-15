587865ce0fe7fdd019afcc36165016169f3a96aa
aalsberge <aalsberge@homeaway.com>
2022-09-14 19:07:03 -0500

    broken out to separate repos

--- admin/rest-api/config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-    devAdminURL: "http://localhost:3001",
-    prodAdminURL: "https://admin.alsberge.dev",
-    devFrontendWebsiteURL: "http://localhost:3000",
-    prodFrontendWebsiteURL: "https://alsberge.dev"
-}
-
-module.exports = config
