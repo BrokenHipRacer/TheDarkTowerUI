8ffb60e7588f5ab9703320511ee54b34a8af0709
aalsberge <aalsberge@homeaway.com>
2022-09-18 20:32:27 -0500

    moved public files back

--- src/components/headMetadata.js
+++ src/components/headMetadata.js
@@ -14,7 +14,7 @@ export default class headMetadata extends Component {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="apple-touch-icon" sizes="180x180"
           href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32"
+        <link rel="icon" type="image/jpg" sizes="32x32"
           href="/tiny_logo_32_size.jpg" />
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
